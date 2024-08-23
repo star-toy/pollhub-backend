a183dcc2c8eb36890ecc1f9337421062f3698460
bloomspes <bloomspes@gmail.com>
2024-08-23 22:43:20 +0900

    Init project

---
+++ src/main/java/com/startoy/pollhub/PollhubApplication.java
@@ -0,0 +1,11 @@
+package com.startoy.pollhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollhubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PollhubApplication.class, args);
+	}
+}
