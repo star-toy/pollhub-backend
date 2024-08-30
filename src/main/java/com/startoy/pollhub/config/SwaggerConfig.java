cca358b491af8b1af346fb8175e35d3331c78b91
park.se.eun2209@gmail.com <park.se.eun2209@gmail.com>
2024-08-30 19:01:05 +0900

    Add initial SwaggerConfig.java

---
+++ src/main/java/com/startoy/pollhub/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+// src/main/java/com/startoy/pollhub/config/SwaggerConfig.java
+package com.startoy.pollhub.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Pollhub API")
+                        .version("1.0.0")
+                        .description("Pollhub API Document"));
+    }
+}
