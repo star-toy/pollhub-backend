ec8ddd7b39ba200bafc068724e8f73c532a419af
park.se.eun2209@gmail.com <park.se.eun2209@gmail.com>
2024-09-04 20:58:46 +0900

    fix : ph_post 테이블명 수정

--- src/main/java/com/startoy/pollhub/domain/Post.java
+++ src/main/java/com/startoy/pollhub/domain/Post.java
@@ -1,19 +1,15 @@
 package com.startoy.pollhub.domain;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Entity
-@Builder
-@Table(name = "posts")
-public class Post  {
+@Table(name = "ph_post")
+public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,18 +19,17 @@ public class Post  {
     @Column(nullable = false, length = 200)
     private String title;
 
+    @Column(name = "poll_id", nullable = false)
+    private Long pollId;
+
     @Column(name = "file_id")
     private Long fileId;
 
     @Column(name = "is_deleted", nullable = false)
     private Boolean isDeleted;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Poll> polls;
-
-    @Builder.Default     //생성시간 설정
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
     @Column(name = "created_by", length = 20, nullable = false)
     private String createdBy;
@@ -45,4 +40,6 @@ public class Post  {
     @Column(name = "updated_by", length = 20)
     private String updatedBy;
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Poll> polls;
 }
