594852abd0809796a9058967b8dd9d862c646ef3
poeekim <hyeonakim0129@gmail.com>
2024-09-04 14:32:38 +0900

    fix : add

--- src/main/java/com/startoy/pollhub/domain/PollOption.java
+++ src/main/java/com/startoy/pollhub/domain/PollOption.java
@@ -2,7 +2,7 @@ package com.startoy.pollhub.domain;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
--- src/main/java/com/startoy/pollhub/domain/Post.java
+++ src/main/java/com/startoy/pollhub/domain/Post.java
@@ -2,7 +2,7 @@ package com.startoy.pollhub.domain;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Set;
 
