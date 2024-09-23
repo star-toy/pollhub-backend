ab1a72ab1fafd91f1b8f4e769e01d559d681192e
poeekim <hyeonakim0129@gmail.com>
2024-09-24 02:08:15 +0900

    Chore: S3Service 추가

---
+++ src/main/java/world/startoy/polling/usecase/S3Service.java
@@ -0,0 +1,47 @@
+package world.startoy.polling.usecase;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+
+/*S3 관련 작업(업로드, 삭제 등)을 수행하는 서비스 클래스입니다. S3Config에서 생성한 S3 클라이언트를 사용*/
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadFile(MultipartFile multipartFile) throws IOException {
+        String fileName = createFileName(multipartFile.getOriginalFilename());
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize());
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        amazonS3.putObject(bucket, fileName, multipartFile.getInputStream(), objectMetadata);
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    private String createFileName(String fileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    private String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(String.format("잘못된 형식의 파일 (%s) 입니다", fileName));
+        }
+    }
+}
