74aa69dcd4fa75d51f2c91546d1be6cf5d3f8e9a
seeun-rosy-park <170014875+seeun-rosy-park@users.noreply.github.com>
2024-09-24 20:35:49 +0900

    Fix: 미사용 코드 제거

--- src/main/java/world/startoy/polling/adapter/controller/VoteController.java
+++ src/main/java/world/startoy/polling/adapter/controller/VoteController.java
@@ -1,17 +1,15 @@
 package world.startoy.polling.adapter.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.*;
-import world.startoy.polling.usecase.UserService;
-import world.startoy.polling.usecase.VoteService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import world.startoy.polling.usecase.dto.VoteDTO;
-import world.startoy.polling.usecase.dto.VoteOptionRateDTO;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import world.startoy.polling.usecase.UserService;
+import world.startoy.polling.usecase.VoteService;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,7 +21,7 @@ public class VoteController {
     private final UserService userService;
 
 
-/*    @PostMapping
+    @PostMapping
     public ResponseEntity<String> submitVote(@RequestParam Long pollId, @RequestParam Long optionId, HttpServletRequest request) {
         String voterIp = userService.getClientIp(request);
 
@@ -34,23 +32,8 @@ public class VoteController {
             System.out.println(e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-    }*/
-
-    @PostMapping("/{pollUid}")
-    @Operation(summary = "투표와 동시에 투표율 조회")
-    public ResponseEntity<List<VoteOptionRateDTO>> submitAndGetVoteRates(String pollUid, @RequestBody VoteDTO voteDTO) {
-        // 1. 투표 제출
-        voteService.submitVote(pollUid, voteDTO);
-
-        // 2. 투표율 조회
-        List<VoteOptionRateDTO> voteRates = voteService.getVoteRates(voteDTO.getPollUid());
-
-        // 3. 결과 반환
-        return ResponseEntity.ok(voteRates);
-    } //catch (IllegalStateException e) {
-       // return ResponseEntity.badRequest().body(null);
     }
 
-//}
+}
 
 
--- src/main/java/world/startoy/polling/usecase/VoteService.java
+++ src/main/java/world/startoy/polling/usecase/VoteService.java
@@ -6,11 +6,9 @@ import org.springframework.stereotype.Service;
 import world.startoy.polling.adapter.repository.PollOptionRepository;
 import world.startoy.polling.adapter.repository.VoteRepository;
 import world.startoy.polling.domain.Vote;
-import world.startoy.polling.usecase.dto.OptionVoteRateDTO;
 import world.startoy.polling.usecase.dto.PollOptionResponse;
 
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -44,40 +42,6 @@ public class VoteService {
         return !voteRepository.findByPollIdAndVoterIp(pollId, voterIp).isEmpty();
     }
 
-
-    // 투표율 조회
-    public List<OptionVoteRateDTO> getVoteRates(Long pollId) {
-        // 특정 pollId에 대한 모든 투표 가져오기
-        List<Vote> votes = voteRepository.findByPollId(pollId);
-
-        // 전체 투표 수 계산
-        long totalVotes = votes.size();
-
-        // 각 optionId별 투표 수 집계
-        /*
-        * Java Stream API 를 이용 Vote 엔티티 리스트를 optionId를 기준으로 그룹화하고
-        * 각 그룹의 투표 수를 계산하여 Map 형태로 반환
-        * */
-        Map<Long, Long> votesPerOption = votes.stream()
-                .collect(Collectors.groupingBy(Vote::getOptionId, Collectors.counting()));
-
-        // 각 optionId에 해당하는 OptionVoteRate 생성
-        return votesPerOption.entrySet().stream()
-                .map(entry -> {
-                    // PollOption 데이터에서 UID와 텍스트 가져오기
-                    Long optionId = entry.getKey(); // 백엔드에서 사용되는 PollOption의 ID
-                    var pollOption = pollOptionRepository.findById(optionId)
-                            .orElseThrow(() -> new IllegalStateException("옵션 ID를 찾을 수 없습니다: " + optionId));
-
-                    return new OptionVoteRateDTO(
-                            pollOption.getPollOptionUid(),                // API 응답에 사용될 PollOptionUid
-                            pollOption.getPollOptionText(),               // optionText
-                            entry.getValue(),                             // votes
-                            totalVotes == 0 ? 0 : (entry.getValue() * 100.0 / totalVotes) // voteRate
-                    );
-                })
-                .collect(Collectors.toList());
-    }
 
     public List<PollOptionResponse> getVoteCountByPollId(Long pollId) {
         List<Tuple> pollOptions = voteRepository.countVotesByPollId(pollId);
