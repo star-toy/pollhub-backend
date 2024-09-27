631edd4337ecc57ced240cb31ed24e31302f00a7
seeun-rosy-park <170014875+seeun-rosy-park@users.noreply.github.com>
2024-09-27 17:41:00 +0900

    Feat: findAllPostWithFile() 추가

--- src/main/java/world/startoy/polling/adapter/repository/PostRepository.java
+++ src/main/java/world/startoy/polling/adapter/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package world.startoy.polling.adapter.repository;
 
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import world.startoy.polling.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import world.startoy.polling.usecase.dto.PostDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +17,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByPostUid(String postUid);
 
     List<Post> findAll();
+
+    @Query("SELECT new world.startoy.polling.usecase.dto.PostDTO(p.postUid, p.title, f.fileUid, f.fileFullName, p.createdBy, p.createdAt) " +
+            "FROM Post p LEFT JOIN p.file f")
+    List<PostDTO> findAllPostWithFile();
 }
