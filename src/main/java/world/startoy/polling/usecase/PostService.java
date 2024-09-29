e4c228f56eb582218f3d1d1050fb5237a25a5347
poeekim <hyeonakim0129@gmail.com>
2024-09-29 17:08:03 +0900

    fix: PostService 게시글 생성 수정

--- src/main/java/world/startoy/polling/usecase/PostService.java
+++ src/main/java/world/startoy/polling/usecase/PostService.java
@@ -179,6 +179,7 @@ public class PostService {
     }
 
 
+    @Transactional
     public PostCreateResponse createPost(PostCreateRequest request, String createdBy) {
         String newPostUid = UUID.randomUUID().toString();
 
