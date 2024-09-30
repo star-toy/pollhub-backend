f1d82edeb05595d8a85f67510693b410ee21a1dc
seeun-rosy-park <170014875+seeun-rosy-park@users.noreply.github.com>
2024-09-30 22:28:31 +0900

    Feat: 주 엔티티를 Vote -> PollOption 수정 - PollOption 에는 데이터가 항상 있지만, Vote 에는 데이터가 없을 수 있음 - 따라서 Vote 에 없는 선택지 목록은 조회되지 않는 오류 발생 - file 데이터 null -> '' 처리 - 반환값 Tuple -> PollOptionResponse 수정

--- src/main/java/world/startoy/polling/adapter/repository/VoteRepository.java
+++ src/main/java/world/startoy/polling/adapter/repository/VoteRepository.java
@@ -1,11 +1,11 @@
 package world.startoy.polling.adapter.repository;
 
-import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import world.startoy.polling.domain.Vote;
+import world.startoy.polling.usecase.dto.PollOptionResponse;
 
 import java.util.List;
 
@@ -17,20 +17,19 @@ public interface VoteRepository extends JpaRepository<Vote, Long> {
     List<Vote> findByPollId(Long pollId);
 
     // 투표 옵션 및 득표수 가져오는 쿼리
-    @Query("SELECT " +
-            "o.pollOptionUid as pollOptionUid, " +
-            "o.pollOptionText as pollOptionText, " +
-            "o.pollOptionSeq as pollOptionSeq, " +
-            "f.fileUid as fileUid, " +
-            "f.fileFullName as fileFullName, " +
-            "COUNT(v.id) as votedCount " +
-            "FROM Vote v " +
-            "JOIN PollOption o ON v.optionId = o.id " +
-            "LEFT JOIN FileStorage f ON o.file.id = f.id " +  // FileStorage와 JOIN
-            "WHERE v.pollId = :pollId " +
+    @Query("SELECT new world.startoy.polling.usecase.dto.PollOptionResponse( " +
+            "o.pollOptionUid, " +
+            "o.pollOptionSeq, " +
+            "o.pollOptionText, " +
+            "COUNT(v.id), " +
+            "COALESCE(f.fileUid, ''), " +  // null이면 빈 문자열로 대체
+            "COALESCE(f.fileFullName, '')) " +  // null이면 빈 문자열로 대체
+            "FROM PollOption o " +
+            "LEFT JOIN Vote v ON o.id = v.optionId " +
+            "LEFT JOIN FileStorage f ON o.file.id = f.id " +
+            "WHERE o.poll.id = :pollId " +
             "GROUP BY o.pollOptionUid, o.pollOptionText, o.pollOptionSeq, f.fileUid, f.fileFullName")
-    List<Tuple> countVotesByPollId(@Param("pollId") Long pollId);
-
+    List<PollOptionResponse> findPollOptionsWithVoteCount(@Param("pollId") Long pollId);
 
 }
 
