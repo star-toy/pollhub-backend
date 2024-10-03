b0365289d46e2ec3e1a2217f87e454effbe528a3
seeun-rosy-park <170014875+seeun-rosy-park@users.noreply.github.com>
2024-10-03 22:32:50 +0900

    Feat: hasVoted 사용자 투표 여부 컬럼 추가, 사용자 투표 여부 없는 생성자 추가

--- src/main/java/world/startoy/polling/usecase/dto/PollOptionResponse.java
+++ src/main/java/world/startoy/polling/usecase/dto/PollOptionResponse.java
@@ -20,5 +20,16 @@ public class PollOptionResponse {
     @NotNull
     private Long votedCount; // 득표수 // JPQL 쿼리에서 COUNT(v.id)는 Long 타입을 반환
     @NotNull
+    private boolean hasVoted; // 해당 옵션 사용자 투표 여부
+    @NotNull
     private String imageUrl; // CloudFront URL을 포함한 전체 이미지 URL
+
+
+    public PollOptionResponse(String pollOptionUid, int pollOptionSeq, String pollOptionText, Long votedCount, String imageUrl) {
+        this.pollOptionUid = pollOptionUid;
+        this.pollOptionSeq = pollOptionSeq;
+        this.pollOptionText = pollOptionText;
+        this.votedCount = votedCount;
+        this.imageUrl = imageUrl;
+    }
 }
