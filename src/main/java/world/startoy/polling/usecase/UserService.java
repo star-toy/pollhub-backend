c8485c7644191c01726a7173fb25f03045eaaf16
seeun-rosy-park <170014875+seeun-rosy-park@users.noreply.github.com>
2024-10-03 17:37:57 +0900

    Feat: 더 명확한 사용자 ip 조회를 위해 로직 수정

--- src/main/java/world/startoy/polling/usecase/UserService.java
+++ src/main/java/world/startoy/polling/usecase/UserService.java
@@ -11,9 +11,31 @@ public class UserService {
     // 사용자 ip 조회
     public String getClientIp(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
+        if (ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip)) {
+            // X-Forwarded-For 헤더의 첫 번째 IP만 가져오기
+            return ip.split(",")[0];
         }
-        return ip;
+
+        ip = request.getHeader("Proxy-Client-IP");
+        if (ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+
+        ip = request.getHeader("WL-Proxy-Client-IP");
+        if (ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+
+        ip = request.getHeader("HTTP_CLIENT_IP");
+        if (ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+
+        ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        if (ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+
+        return request.getRemoteAddr();
     }
 }
